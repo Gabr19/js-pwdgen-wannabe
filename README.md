45fe9123a6b0c2d022d46feb5ca572839aa9fa64
Gabr19 <sannagabriele19@gmail.com>
2022-12-16 16:13:56 +0100

    Richiesta informazioni utente

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Chiedi all'utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21
+
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const name = prompt ('come ti chiami?')
+console.log("L'utente si chiama: ", name );
+
+const surname = prompt ("Qual'è il tuo cognome?")
+console.log("Il cognome dell'utente è: ", surname );
+
+const color = prompt ("Qual'è il tuo colore preferito?")
+console.log("Il colore preferito dell'utente è: ", color );
+
