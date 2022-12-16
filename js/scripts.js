910617f05894cbf53f25151af1027bfa6ab3fc94
Gabr19 <sannagabriele19@gmail.com>
2022-12-16 17:27:58 +0100

    fine lavoro

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 const name = prompt ('come ti chiami?')
 console.log("L'utente si chiama: ", name );
 
-const surname = prompt ("Qual'è il tuo cognome?")
-console.log("Il cognome dell'utente è: ", surname );
+ const surname = prompt ("Qual'è il tuo cognome?")
+ console.log("Il cognome dell'utente è: ", surname );
 
 const color = prompt ("Qual'è il tuo colore preferito?")
 console.log("Il colore preferito dell'utente è: ", color );
 
+
+
+
+document.getElementById("text").innerHTML = name + surname + color + 21;
